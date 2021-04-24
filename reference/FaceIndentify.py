6308b43886612ff88905fb64be6bc62325ea4c60
Kaige <zhukaige2000@outlook.com>
2021-04-24 18:46:36 +0800

    测试用

--- reference/FaceIndentify.py
+++ reference/FaceIndentify.py
@@ -9,13 +9,13 @@ unknown_image = face_recognition.load_image_file("C:\\Users\\Kaige\\OneDrive\\�
 
 #获取每个图像文件中每个面部的面部编码
 #由于每个图像中可能有多个面，所以返回一个编码列表。
-konwn_face_encoding = face_recognition.face_encodings(konwn_image)
-unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+konwn_face_encoding = face_recognition.face_encodings(konwn_image)[0]
+
+
 print(konwn_face_encoding)
-print(unknown_face_encoding[0])
+
 #print("unknown_face_encoding :{}".format(unknown_face_encoding))
 
 #结果是True/false的数组，
-results = face_recognition.compare_faces(konwn_face_encoding, unknown_face_encoding)
 
-print(results)
+
