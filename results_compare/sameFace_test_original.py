afc17ba9a8c4b8f5a43456e0fa0a1e9dfedc8164
Kaige <zhukaige2000@outlook.com>
2021-04-24 18:47:09 +0800

    修改拼写错误，无功能更新

--- results_compare/sameFace_test_original.py
+++ results_compare/sameFace_test_original.py
@@ -63,7 +63,7 @@ Path = 'C:\\Users\\Kaige\\OneDrive\\学习\\毕业设计\\FaceDetect\\att-databa
 print("same face test start")
 test_num, detect_error, detect_error_rate, identify_error, identify_error_rate = same_face_test(Path, 0.6)
 print('the num of sample is %d' % test_num)
-print('dectect error num is %d, detect error rate is %s' % (detect_error, detect_error_rate))
-print('indentify error num is %d, indentify error rate is %s' % (identify_error, identify_error_rate))
+print('detect error num is %d, detect error rate is %s' % (detect_error, detect_error_rate))
+print('identify error num is %d, identify error rate is %s' % (identify_error, identify_error_rate))
 
 
